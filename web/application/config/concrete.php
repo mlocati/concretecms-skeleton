318ade3ed5c76b3a066465aa0fd97c9450726cb6
Michele Locati <michele@locati.it>
2023-01-24 09:05:00 +0100

    Add a not about Concrete packages installed with Composer

--- web/application/config/concrete.php
+++ web/application/config/concrete.php
@@ -3,5 +3,8 @@
 return [
     'updates' => [
         'skip_core' => true,
+        'skip_packages' => [
+            // Add here the Concrete handles of the packages that you install and update with Composer
+        ],
     ],
 ];
